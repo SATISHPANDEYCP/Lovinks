7d45fbf9e3c8cd1d0dbfa297d2b739311178b71d
satishpandeycp <satishpandeycp@gmail.com>
2025-05-01 19:51:53 +0530

    improvement

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:"https://lovink.onrender.com",
+  baseURL:"https://lovink.onrender.com/api",
   withCredentials: true,
 });
